188267049a2398e71af4dfcfa70823e40ca00653
jonathan-r-jones <jonathan.r.jones.3@gmail.com>
2020-11-30 00:48:53 -0500

    Nov-30-2020_12:48_AM

--- src/test/java/com/yb/bowling/BowlingGameTest.java
+++ src/test/java/com/yb/bowling/BowlingGameTest.java
@@ -146,9 +146,11 @@ public class BowlingGameTest {
         String input = "9/-/XX627/8/X9-9/9";
         assertEquals(167, game.scoreOfGame(input));
     }
+
+    // This is a bug in the code. 168 seems to be
+    // an impossible game according this test!
     @Test
     public void FinalBallBug_168(){
-        // This is a bug in the code. 168 is impossible!
         String input = "9/-/XX627/8/X9-9/X";
         assertEquals(168, game.scoreOfGame(input));
     }
